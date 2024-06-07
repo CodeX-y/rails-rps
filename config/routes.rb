c9756c8522854d947d837a92801a2db355f80294
CodeX-y <84106371+CodeX-y@users.noreply.github.com>
2024-06-07 23:05:57 +0000

    passed, but buttons not on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get("/rock", {:controller=>"game",:action =>"rock"}) 
+  get("/paper", {:controller=>"game",:action =>"paper"}) 
+  get("/scissors", {:controller=>"game",:action =>"scissors"})
+  get("/", {:controller=>"game",:action =>"home"})
 
 end
